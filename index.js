c1b0cc5553a4d8b5c909b811bcd1913ae4b6a309
ihassan07 <103757343+ihassan07@users.noreply.github.com>
2023-08-10 13:18:43 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function sun() {
+    document.getElementById("hedoo").innerHTML = "علي ايزي";
+    alert("علي ايزي");
+    /*
+    document.getElementById("hedoo").innerHTML = document.getElementById("textbar2").innerHTML;
+
+    let password;
+ 
+document.getElementById("send").onclick = function () {
+    password = document.getElementById("enterpass2").value;
+
+
+if (password == "St123456/Aliezi") {
+    alert("علي ايزي");
+
+    
+}}*/
+}
